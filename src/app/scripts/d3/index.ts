07e38e1f383baec4540d15406b4565240695a681
Alex Lockwood <alexjlockwood@gmail.com>
2017-07-07 15:00:25 -0400

    finish triangulation demo

--- src/app/scripts/d3/index.ts
+++ src/app/scripts/d3/index.ts
@@ -4,4 +4,8 @@ export { feature, object } from './feature';
 export { mergeArcs } from './mergeArcs';
 export { neighbors } from './neighbors';
 export { polygonArea } from './polygonArea';
+export { polygonCentroid } from './polygonCentroid';
+export { polygonLength } from './polygonLength';
+export { range } from './range';
 export { shuffle } from './shuffle';
+export { sum } from './sum';
---
+++ src/app/scripts/d3/range.ts
@@ -0,0 +1,14 @@
+export function range(start, stop?, step?) {
+  (start = +start), (stop = +stop), (step =
+    (n = arguments.length) < 2 ? ((stop = start), (start = 0), 1) : n < 3 ? 1 : +step);
+
+  var i = -1,
+    n = Math.max(0, Math.ceil((stop - start) / step)) | 0,
+    rng = new Array(n);
+
+  while (++i < n) {
+    rng[i] = start + i * step;
+  }
+
+  return rng;
+}
